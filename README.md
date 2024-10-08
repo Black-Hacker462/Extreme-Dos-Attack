7665665ec5b8832662326e6028e516f2c2b720b3
Black-Hacker462 <brovaiakash5903@gmail.com>
2024-10-08 20:32:30 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+#DDOS 
+Paid and Unlimited best DDOS ATTACK Tools
+<center><h3>🖤Assalamu Alaikum🖤</h3></center>
+
+<h2>INSTALLATION ⬇️</h2>
+
+
+
+```
+pkg update -y
+pkg upgrade -y
+pkg install git -y
+pkg install python -y
+pkg install python3 -y
+git clone https://github.com/cyweb/hammer
+git clone https://github.com/Black-Hacker462/Extreme-Dos-Attack
+cd Extreme-Dos-Attack
+chmod +x dos.sh
+./dos
+```
---
+++ dos.sh
@@ -0,0 +1,30 @@
+GNU nano 8.2                                   Dos.sh
+#!/bin/bash
+clear
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
+   echo -e "${RED} ____  _        _    ____ _  __  _   _    _    ____ _  _______ ____ ${RESET}"
+ echo -e "${GREEN}| __ )| |      / \  / ___| |/ / | | | |  / \  / ___| |/ / ____|  _ \ ${RESET} "
+echo -e "${YELLOW}|  _ \| |     / _ \| |   | ' /  | |_| | / _ \| |   | ' /|  _| | |_) | ${RESET} "
+  echo -e "${BLUE}| |_) | |___ / ___ \ |___| . \  |  _  |/ ___ \ |___| . \| |___|  _ < ${RESET}"
+   echo -e "${RED}|____/|_____/_/   \_\____|_|\_\ |_| |_/_/   \_\____|_|\_\_____|_| \_\ ${RESET} "
+ echo -e "${GREEN} ===================================================================== ${RESET}"
+     echo -e " ${RED} [√] Author       :SASUKE "
+   echo -e " ${GREEN} [√] Tool Type    :DOS ATTACK "
+echo -e " ${BLUE} [√] Paid Or Free :PAID "
+  echo -e " ${YELLOW} [√] VERSION      :1.2"
+    echo -e " ${BLUE} [√] TEAM         :TEAM-BLACK-HACKER "
+echo -e " ${BLUE}====================================================================="
+echo -e " ${GREEN}"
+read -p  "Enter Password :"
+cd hammer
+echo "usage :cd hammer ; python3 hammer.py [-s] [-p] [-t]"
+echo "   -h : help"
+echo "   -s : server ip"
+  echo "   -p : port default 80"
+  echo "   -t : turbo default 135"
